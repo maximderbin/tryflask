27724fb26cafb4cd4a92e197998df251d85947cc
Maxim Derbin <maxim@derbin.io>
2017-03-01 20:52:40 -0600

    Add users#show

--- __init__.py
+++ __init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask import jsonify
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-  return "Hello World!"
+@app.route("/users/<int:id>")
+def show(id):
+  return jsonify(
+    id=id,
+    name="Foo Bar"
+  )
 
 if __name__ == "__main__":
   app.run()
