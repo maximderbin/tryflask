948473d4c6fe3ef346d1e7e18c732b936e816281
Maxim Derbin <maxim@derbin.io>
2017-03-01 23:12:41 -0600

    s/"/'

--- __init__.py
+++ __init__.py
@@ -2,22 +2,22 @@ from flask import Flask
 from flask import jsonify
 app = Flask(__name__)
 
-@app.route("/users")
+@app.route('/users')
 def index():
   return jsonify([
-    { "id": 1, "name": "Foo Bar" },
-    { "id": 2, "name": "Foo Bar" },
-    { "id": 3, "name": "Foo Bar" },
-    { "id": 4, "name": "Foo Bar" },
-    { "id": 5, "name": "Foo Bar" }
+    { 'id': 1, 'name': 'Foo Bar' },
+    { 'id': 2, 'name': 'Foo Bar' },
+    { 'id': 3, 'name': 'Foo Bar' },
+    { 'id': 4, 'name': 'Foo Bar' },
+    { 'id': 5, 'name': 'Foo Bar' }
   ])
 
-@app.route("/users/<int:id>")
+@app.route('/users/<int:id>')
 def show(id):
   return jsonify(
     id=id,
-    name="Foo Bar"
+    name='Foo Bar'
   )
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   app.run()
