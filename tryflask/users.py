f0385dad83c6a4224948d23f0e8f32e2417303ab
Maxim Derbin <maxim@derbin.io>
2017-03-06 22:25:34 -0600

    Add HTTP verbs

--- tryflask/users.py
+++ tryflask/users.py
@@ -2,10 +2,10 @@ from flask import jsonify
 from tryflask import app
 from tryflask.db import users
 
-@app.route('/users')
+@app.route('/users', methods=['GET'])
 def index():
     return jsonify(users())
 
-@app.route('/users/<int:id>')
+@app.route('/users/<int:id>', methods=['GET'])
 def show(id):
     return jsonify(next((x for x in users() if x['id'] == id), None))
