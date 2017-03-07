2fcf7577dc42e6f0c4426d7fe2d97d8fbffc35e2
Maxim Derbin <maxim@derbin.io>
2017-03-06 21:11:25 -0600

    Extract data to separate file

--- __init__.py
+++ __init__.py
@@ -1,23 +1,15 @@
 from flask import Flask
 from flask import jsonify
+from db import users
 app = Flask(__name__)
 
 @app.route('/users')
 def index():
-  return jsonify([
-    { 'id': 1, 'name': 'Foo Bar' },
-    { 'id': 2, 'name': 'Foo Bar' },
-    { 'id': 3, 'name': 'Foo Bar' },
-    { 'id': 4, 'name': 'Foo Bar' },
-    { 'id': 5, 'name': 'Foo Bar' }
-  ])
+  return jsonify(users())
 
 @app.route('/users/<int:id>')
 def show(id):
-  return jsonify({
-    'id': id,
-    'name': 'Foo Bar'
-  })
+  return jsonify(next((x for x in users() if x['id'] == id), None))
 
 if __name__ == '__main__':
   app.run()
---
+++ db.py
@@ -0,0 +1,8 @@
+def users():
+  return [
+    { 'id': 1, 'name': 'Foo Bar' },
+    { 'id': 2, 'name': 'Foo Bar' },
+    { 'id': 3, 'name': 'Foo Bar' },
+    { 'id': 4, 'name': 'Foo Bar' },
+    { 'id': 5, 'name': 'Foo Bar' }
+  ]
